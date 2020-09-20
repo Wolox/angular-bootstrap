fd4e94074532849cb980798d59020423d306053f
Cristian Vega <cristian.vega08@gmail.com>
2020-09-20 18:17:25 -0500

    refactor: use routing and styles variables, add readme notes

--- src/wolox-angular-bootstrap/utils/angular-cli/index.ts
+++ src/wolox-angular-bootstrap/utils/angular-cli/index.ts
@@ -1,10 +1,12 @@
 import { Rule, externalSchematic } from "@angular-devkit/schematics";
 import { options } from "./constants";
 
-export function schematicAngularCLI(name: string): Rule {
+export function schematicAngularCLI(name: string, routing: boolean, style: string): Rule {
   return externalSchematic("@schematics/angular", "ng-new", {
     name,
     directory: name,
+    routing,
+    style,
     ...options,
   });
 }
--- src/wolox-angular-bootstrap/utils/package-json/constants.ts
+++ src/wolox-angular-bootstrap/utils/package-json/constants.ts
@@ -27,4 +27,9 @@ export const dependencies = {
 
 export const removeDependencies = [
   "@types/jasmine",
+  "karma",
+  "karma-chrome-launcher",
+  "karma-coverage-istanbul-reporter",
+  "karma-jasmine",
+  "karma-jasmine-html-reporter"
 ]
