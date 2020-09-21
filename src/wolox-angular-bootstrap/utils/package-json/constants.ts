5874a5d077b4c5c8bfbd8b5bfa005b37c792cb37
Cristian Vega <cristian.vega08@gmail.com>
2020-09-20 22:30:18 -0500

    refactor: delete default versions

--- src/wolox-angular-bootstrap/utils/package-json/constants.ts
+++ src/wolox-angular-bootstrap/utils/package-json/constants.ts
@@ -1,16 +1,4 @@
 export const jestPackage = { 
-  "globals": {
-    "ts-jest": {
-      "astTransformers": {
-        "before": [
-          "jest-preset-angular/build/InlineFilesTransformer",
-          "jest-preset-angular/build/StripStylesTransformer"
-        ]
-      }
-    }
-  },
-  "preset": "jest-preset-angular",
-  "setupFilesAfterEnv": ["<rootDir>/setup-jest.ts"],
   "testPathIgnorePatterns": [
     "<rootDir>/node_modules/",
     "<rootDir>/dist/",
@@ -18,12 +6,14 @@ export const jestPackage = {
   ]
 }
 
-export const dependencies = {
-  "@angular-builders/jest": "^10.0.0",
-  "@types/jest": "^25.2.3",
-  "jest": "^26.0.1",
-  "jest-preset-angular": "^8.2.0",
-}
+
+export const dependencies = [
+  "@angular-builders/jest",
+  "@types/jest",
+  "jest",
+  "jest-preset-angular",
+]
+
 
 export const removeDependencies = [
   "@types/jasmine",
--- src/wolox-angular-bootstrap/utils/package-json/index.ts
+++ src/wolox-angular-bootstrap/utils/package-json/index.ts
@@ -1,23 +1,31 @@
-import { Rule, Tree, SchematicContext, SchematicsException } from "@angular-devkit/schematics";
+import { Rule, Tree, SchematicContext, SchematicsException, chain } from "@angular-devkit/schematics";
 import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
-import { dependencies, removeDependencies } from './constants';
+import { dependencies, removeDependencies, jestPackage } from './constants';
+import { get } from 'http';
 
-export function updatePackageJson(name: string): Rule {
+import { concat, Observable, of } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+
+export interface NodePackage {
+  name: string;
+  version: string;
+}
+
+export function removePackages(name: string): Rule {
   return (tree: Tree, context: SchematicContext): Tree => {
     context.addTask(new NodePackageInstallTask());
     const path = `/${name}/package.json`;
-    if (tree.exists(path)) {
-      const file = tree.read(path);
-      const json = JSON.parse(file!.toString());
-      
+    const json = readJsonFile(tree, path)
+
+    if (json) {      
       // Update scripts
       json.scripts = {
         ...json.scripts,
         test: "jest",
       };
 
-      // Add new dependencies
-      json.dependencies = { ...json.dependencies, ...dependencies };
+      // Add jest config
+      json.jest = { ...jestPackage }
 
       // Remove dependencies
       removeDependencies.forEach((dependency: string) => delete json.devDependencies[dependency])
@@ -27,4 +35,76 @@ export function updatePackageJson(name: string): Rule {
     }
     throw new SchematicsException(`Does not exist ${path}.`);
   };
+}
+
+export function updatePackageJson(name :string): Rule {
+  return chain([
+    removePackages(name),
+    addPackages(name)
+  ]);
+}
+
+function addPackages(name: string): Rule {
+  const path = `/${name}/package.json`;
+  return (tree: Tree, context: SchematicContext): Observable<Tree> => {
+    const newDependencies = of(...dependencies).pipe(
+      concatMap((packageName: any) => getLatestNodeVersion(packageName)),
+      map((packageFromRegistry: NodePackage) => {
+        const { name, version } = packageFromRegistry;
+        context.logger.debug(`Adding ${name}:${version} to Dev Dependencies`);
+        addPackageJsonDependency(tree, context, name, version, path)
+        return tree;
+      })
+    )
+    return concat(newDependencies);
+  }
+}
+
+/**
+ * Attempt to retrieve the latest package version from NPM
+ * Return an optional "latest" version in case of error
+ * @param packageName
+ */
+export function getLatestNodeVersion(packageName: string): Promise<NodePackage> {
+  const DEFAULT_VERSION = 'latest';
+
+  return new Promise((resolve) => {
+    return get(`http://registry.npmjs.org/${packageName}`, (res) => {
+      let rawData = '';
+      res.on('data', (chunk) => (rawData += chunk));
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(rawData);
+          const version = (response && response['dist-tags']) || {};
+
+          resolve(buildPackage(packageName, version.latest));
+        } catch (e) {
+          resolve(buildPackage(packageName));
+        }
+      });
+    }).on('error', () => resolve(buildPackage(packageName)));
+  });
+
+  function buildPackage(name: string, version: string = DEFAULT_VERSION): NodePackage {
+    return { name, version };
+  }
+}
+
+function readJsonFile(tree: Tree, path: string, ) {
+  if (tree.exists(path)) {
+    const file = tree.read(path);
+    return JSON.parse(file!.toString());
+  }
+  return null
+}
+
+
+function addPackageJsonDependency(tree: Tree, context: SchematicContext, name: string, version: string, path: string) {
+  const json = readJsonFile(tree, path);
+  context.addTask(new NodePackageInstallTask());
+  if (json) {
+    json.dependencies = { ...json.dependencies, [name]: `^${version}` };
+  }
+  tree.overwrite(path, JSON.stringify(json, null, 2));
+  return tree;
 }
