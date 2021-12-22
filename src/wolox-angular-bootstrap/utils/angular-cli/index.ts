f78115b0cf8ba62ae9d3e27c71c6d38c81d72466
Elkin Bernal <elkin.bernal@wolox.co>
2021-12-22 16:42:52 -0500

    fix: modify cli version

--- src/wolox-angular-bootstrap/utils/angular-cli/index.ts
+++ src/wolox-angular-bootstrap/utils/angular-cli/index.ts
@@ -2,21 +2,24 @@ import {
   Rule,
   SchematicContext,
   Tree,
-  externalSchematic
+  externalSchematic,
 } from '@angular-devkit/schematics';
-import { PackageManager, Schema as AngularNgNewSchema, Style } from '@schematics/angular/ng-new/schema';
+import {
+  PackageManager,
+  Schema as AngularNgNewSchema,
+  Style,
+} from '@schematics/angular/ng-new/schema';
 
 export function schematicAngularCLI(name: string): Rule {
   return async (_tree: Tree, _context: SchematicContext) => {
-
     const options: AngularNgNewSchema = {
       name,
-      version: '13.1.2',
+      version: '13.x',
       routing: true,
       strict: true,
       style: Style.Scss,
       directory: name,
-      packageManager: PackageManager.Npm
+      packageManager: PackageManager.Npm,
     };
 
     return externalSchematic('@schematics/angular', 'ng-new', options);
